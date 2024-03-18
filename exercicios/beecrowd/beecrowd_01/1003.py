4cbe532c5d3b6786db38cd5dacc7056d4313e5dc
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2024-03-18 17:28:15 -0300

    add

---
+++ exercicios/beecrowd/beecrowd_01/1003.py
@@ -0,0 +1,5 @@
+a = int(input("Digite o valor 1: "))
+b = int(input("Digite o valor 2: "))
+soma = a + b
+
+print("SOMA = ",soma)
---
+++ exercicios/beecrowd/beecrowd_01/1004.py
@@ -0,0 +1,5 @@
+a = int(input("Digite o valor 1: "))
+b = int(input("Digite o valor 2: "))
+prod = a * b
+
+print("PROD = ",prod)
