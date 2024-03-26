75ac8c8af87ccb70f68846e5819cbeec183d4a5f
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-03-26 10:46:23 -0300

    add

--- exercicios/beecrowd/beecrowd_02/1042.py
+++ exercicios/beecrowd/beecrowd_02/1042.py
@@ -1,4 +1,10 @@
-a = int(input())
-b = int(input())
-c = int(input())
+numeros = list(map(int,input().split()))
+var = sorted(numeros)
 
+for i in var:
+    print(f"{i}")
+
+print()
+
+for j in numeros:
+    print(f"{j}")
