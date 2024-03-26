829f3c108210483b60c70925e086fca47d1e5e8c
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-03-26 15:54:51 -0300

    add

--- exercicios/beecrowd/beecrowd_02/1080.py
+++ exercicios/beecrowd/beecrowd_02/1080.py
@@ -1,6 +1,12 @@
-arr = [0]*5
-for i in arr:
-    arr = int(input())
+posicao = 0
+maior_valor = -1
 
-for i in arr:
-    print(arr)
+for i in range(1,101):
+    var = int(input())
+    
+    if maior_valor < var:
+        maior_valor = var 
+        posicao = i
+        
+print(maior_valor)
+print(posicao)
