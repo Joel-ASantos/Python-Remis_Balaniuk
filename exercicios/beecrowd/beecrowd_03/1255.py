0e4c1edbc715f9e2befdf07fe12dd2924a64a19b
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-04-09 14:58:44 -0300

    add

--- exercicios/beecrowd/beecrowd_03/1255.py
+++ exercicios/beecrowd/beecrowd_03/1255.py
@@ -1,32 +1,19 @@
-#Neste problema estamos interessados na frequência das letras em uma dada linha de texto.
-
-#Especificamente, deseja-se saber qual(is) a(s) letra(s) de maior frequência do texto, ignorando o “case sensitive”, 
-# ou seja maiúsculas ou minúsculas (sendo mais claro, “letras” referem-se precisamente às 26 letras do alfabeto).
-
-#Entrada
-#A entrada contém vários casos de teste. A primeira linha contém um inteiro N que indica a quantidade de casos de teste. 
-# Cada caso de teste consiste de uma única linha de texto. A linha pode conter caracteres “não letras”, 
-# mas é garantido que tenha ao menos uma letra e que tenha no máximo 200 caracteres no total.
-
-#Saída
-#Para cada caso de teste, imprima uma linha contendo a(s) letra(s) que mais ocorreu(ocorreram) no texto em minúsculas
-# (se houver empate, imprima as letras em ordem alfabética).
-
 def letras_freq(texto):
     freq = {}
     for char in texto:
-        if char in freq:
-            if char.isalpha():
+        if char.isalpha():
+            if char in freq:
                 freq[char] += 1
-        else:
-            freq[char] = 1
-    maxOcorr = max(freq.values())
-    letras_maxOcorr = [letra for letra, freq in freq.items() if freq == maxOcorr]
-    letras_maxOcorr.sort()
-    return letras_maxOcorr
-            
+            else:
+                freq[char] = 1
+                
+    max_ocorr = max(freq.values())
+    letras_max_ocorr = [letra for letra, freq in freq.items() if freq == max_ocorr]
+    letras_max_ocorr.sort()
+    return letras_max_ocorr
+
 teste = int(input())
 for i in range(teste):
     texto = input().lower()
-    resul=letras_freq(texto)
+    resul = letras_freq(texto)
     print(''.join(resul))
